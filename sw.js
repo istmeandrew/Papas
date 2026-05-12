ddb3c2bce3a26a3ec7292ba74563d16706917d75
istmeandrew <70925220+istmeandrew@users.noreply.github.com>
2026-05-12 15:35:24 -0400

    Activate Papas cache update immediately

--- sw.js
+++ sw.js
@@ -28,12 +28,15 @@ const TIME_FIX = `
 `;
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES)));
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
 
